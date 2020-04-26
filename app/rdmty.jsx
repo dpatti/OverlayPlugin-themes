0f78c0d7671c773653c405ffaeb7b88300d216f5
Doug Patti <douglas.patti@gmail.com>
2020-04-26 16:54:32 -0400

    fix number formatting

--- app/rdmty.jsx
+++ app/rdmty.jsx
@@ -31,11 +31,10 @@ var formatName = (name) => {
 var formatNumber = (number) => {
     number = parseFloat(number, 10);
 
-    if (number >= 1000) {
-        return (number / 1000).toFixed(2) + 'K';
-    }
-    else if (number >= 1000000) {
+    if (number >= 1000000) {
         return (number / 1000000).toFixed(2) + 'M';
+    } else if (number >= 1000) {
+        return (number / 1000).toFixed(2) + 'K';
     }
 
     return number.toFixed(2);
